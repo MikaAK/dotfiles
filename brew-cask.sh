22fdeaf654fa26d843215a7057ab258ab3e31a9d
MikaAK <mika@kalathil.me>
2017-10-05 00:32:24 -0700

    feat: Add macos startup scripts and brew installs

---
+++ brew-cask.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+brew cask install -y alfred dash iconjar radiant-player slack \
+                     bartender google-chrome iterm2 setapp \
+                     boom-3d graphiql kitematic sizeup \
+                     classeur
---
+++ brew.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Install Ruby
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+source ~/.bash_profile
+
+brew update &&
+brew doctor &&
+
+# Install brew packages and fish
+brew install -y fish postgresql coreutils automake autoconf openssl libyaml readline libxslt libtool unixodbc &&
+sudo bash -c "echo '/usr/local/bin/fish' >> /etc/shells" &&
+sudo chsh -s /usr/local/bin/fish &&
+
+# Install ASDF
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.4.0 &&
+echo 'source ~/.asdf/asdf.fish' >> ~/.config/fish/config.fish &&
+mkdir -p ~/.config/fish/completions &&
+cp ~/.asdf/completions/asdf.fish ~/.config/fish/completions &&
+
+# Install ASDF plugins
+fish --command "asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git" &&
+fish --command "asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git" &&
+fish --command "asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git" &&
+
+# Imports Node.js release team's OpenPGP keys to main keyring
+bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring &&
+
+# Install Node/Elixir verisons
+fish --command "asdf install erlang 20.1" &&
+fish --command "asdf global erlang 20.1" &&
+fish --command "asdf install elixir 1.5.1" &&
+fish --command "asdf global elixir 1.5.1" &&
+fish --command "asdf install nodejs 8.6.0" &&
+fish --command "asdf global nodejs 8.6.0"
