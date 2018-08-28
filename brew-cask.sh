28415e0a99bf7b09041a099d864692c75f33bff7
Mika Kalathil <mika@kalathil.me>
2018-08-28 02:06:55 -0700

    Delete brew-cask.sh

--- brew-cask.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-brew cask install -y alfred dash iconjar radiant-player slack \
-                     bartender google-chrome iterm2 \
-                     boom-3d graphiql sizeup \
-                     classeur
